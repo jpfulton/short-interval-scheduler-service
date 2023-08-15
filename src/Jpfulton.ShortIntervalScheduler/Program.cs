33a80487af9edb344fd4a597fee5c131b7f0f85b
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-08-15 16:13:48 +0000

    Enhance logging.

--- src/Jpfulton.ShortIntervalScheduler/Program.cs
+++ src/Jpfulton.ShortIntervalScheduler/Program.cs
@@ -2,16 +2,6 @@ using Jpfulton.ShortIntervalScheduler;
 using Microsoft.Extensions.Logging.Configuration;
 using Microsoft.Extensions.Logging.EventLog;
 
-Options? workerOptions = null;
-try
-{
-    workerOptions = Options.GetOptionsFromArgs(args);
-}
-catch (Exception)
-{
-    Environment.Exit(1);
-}
-
 HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddWindowsService(options =>
 {
@@ -20,6 +10,18 @@ builder.Services.AddWindowsService(options =>
 
 LoggerProviderOptions.RegisterProviderOptions<
     EventLogSettings, EventLogLoggerProvider>(builder.Services);
+
+ILogger logger = builder.Services.BuildServiceProvider().GetRequiredService<ILogger<Program>>();
+Options? workerOptions = null;
+try
+{
+    workerOptions = Options.GetOptionsFromArgs(args);
+}
+catch (Exception e)
+{
+    logger.LogError(e, "Unable to build service options.");
+    Environment.Exit(1);
+}
 
 builder.Services.AddSingleton(workerOptions);
 builder.Services.AddHostedService<Worker>();
--- src/Jpfulton.ShortIntervalScheduler/Worker.cs
+++ src/Jpfulton.ShortIntervalScheduler/Worker.cs
@@ -15,6 +15,10 @@ public class Worker : BackgroundService
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        _logger.LogInformation("Starting service execution loop.");
+        _logger.LogInformation($"Using delay between command runs: {_options.DelayInSeconds}");
+        _logger.LogInformation($"Using command: '{_options.Command}'.");
+
         var cmdArray = _options.Command.Split(" ");
         var cmd = cmdArray[0];
 
